333bc1ef2778e9170f364edc2870e6a44abecde9
Theo Kleman <theo@kleman.fr>
2016-04-20 15:13:37 +0200

    Add funny console.log

--- js/app/main.js
+++ js/app/main.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 
     app = new App();
     app.init();
-
+    console.log("Nothing to see here :(");
+    console.log("If you wanna see the code behind this website, I suggest you to take a look on the Github repo : https://github.com/TheoKleman/theokleman");
 });
